af543359ea75503e024dd6c47e5e1da09fa7978d
Oliver Freyermuth <o.freyermuth@googlemail.com>
2021-01-14 16:37:24 +0100

    bla

--- .github/workflows/gen-pandoc.sh
+++ .github/workflows/gen-pandoc.sh
@@ -1,6 +1,7 @@
 for index in find -name "_index.md"; do
   MOD_DIR=$(dirname "${index}")
   MOD_NAME=$(basename "${MOD_DIR}")
-  pushd "${MOD_DIR}"
+  cd "${MOD_DIR}"
   pandoc _index.md -o "${MOD_NAME}.pdf"
+  cd -
 done
